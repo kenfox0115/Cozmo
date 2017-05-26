321c59ca7761f4e3ad0fa22b27050d32e4fbbdb7
kenfox0115 <ken.c.fox@gmail.com>
2017-05-25 19:27:55 -0700

    Update basic_sneezing.py

--- basic_sneezing.py
+++ basic_sneezing.py
@@ -1,12 +1,16 @@
 import cozmo
+from cozmo.util import distance_mm, speed_mmps
+import time
 
 def cozmo_program_sneeze(robot: cozmo.robot.Robot):
-    robot.drive_straight(distance_mm(-50), speed_mmps(100))
-    robot.say_text("aa").wait_for_completed()
-    time.sleep(1)
-    robot.say_text("aa").wait_for_completed()
-    time.sleep(1)
-    robot.drive_straight(distance_mm(50), speed_mmps(100))
+    robot.drive_straight(distance_mm(-50), speed_mmps(100)).wait_for_completed()
+    robot.move_head(5)
+    robot.say_text("ah").wait_for_completed()
+    robot.drive_straight(distance_mm(-50), speed_mmps(100)).wait_for_completed()
+    robot.move_head(5)
+    robot.say_text("ah").wait_for_completed()
+    robot.drive_straight(distance_mm(50), speed_mmps(100)).wait_for_completed()
+    robot.move_head(-10)
     robot.say_text("aachoo").wait_for_completed()
     anim = robot.play_anim_trigger(cozmo.anim.Triggers.DizzyReactionMedium)
     anim.wait_for_completed()
